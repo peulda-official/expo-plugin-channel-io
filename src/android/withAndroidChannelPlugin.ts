3c21c7d66ed29806929418d27687d5ce2452cbec
Ryo Kuramoto <dekachan16@gmail.com>
2024-07-24 17:32:57 +0900

    feat: Support Kotlin MainActivity files (#1)
    * feat: Support Kotlin MainActivity files
    
    * chore: Upgrade packages & Run expo prebuild
    
    * chore: Add babel-plugin-module-resolver

--- src/android/withAndroidChannelPlugin.ts
+++ src/android/withAndroidChannelPlugin.ts
@@ -55,6 +55,56 @@ function modifyJavaMainActivity(contents: string) {
   return lines.join('\n')
 }
 
+function addKotlinImports(
+  kotlinSource: string,
+  kotlinImports: string[]
+): string {
+  const lines = kotlinSource.split('\n')
+  const lineIndexWithPackageDeclaration = lines.findIndex((line) =>
+    line.match(/^package .*$/)
+  )
+  for (const kotlinImport of kotlinImports) {
+    if (!kotlinSource.includes(kotlinImport)) {
+      const importStatement = `import ${kotlinImport}`
+      lines.splice(lineIndexWithPackageDeclaration + 1, 0, importStatement)
+    }
+  }
+  return lines.join('\n')
+}
+
+function modifyKotlinMainActivity(contents: string) {
+  contents = addKotlinImports(contents, [
+    'com.zoyi.channel.plugin.android.ChannelIO',
+  ])
+
+  const lines = contents.split('\n')
+  const initialized = lines.some((line) =>
+    line.includes('ChannelIO.initialize(this)')
+  )
+  if (initialized) {
+    return contents
+  }
+
+  const onApplicationCreateIndex = lines.findIndex((line) =>
+    line.includes('ApplicationLifecycleDispatcher.onApplicationCreate(this)')
+  )
+
+  if (onApplicationCreateIndex === -1) {
+    WarningAggregator.addWarningAndroid(
+      '@peulda/expo-plugin-channel-io',
+      'Failed to add ChannelIO.initialize(this) to MainActivity.kt. You will need to manually add it to your MainActivity.kt'
+    )
+    return contents
+  }
+
+  lines.splice(
+    onApplicationCreateIndex + 1,
+    0,
+    '    ChannelIO.initialize(this)'
+  )
+  return lines.join('\n')
+}
+
 export const withAndroidChannelPlugin: ConfigPlugin<ChannelPluginProps> = (
   config: ExpoConfig
 ) => {
@@ -64,9 +114,8 @@ export const withAndroidChannelPlugin: ConfigPlugin<ChannelPluginProps> = (
         config.modResults.contents
       )
     } else {
-      WarningAggregator.addWarningAndroid(
-        '@peulda/expo-plugin-channel-io',
-        `Kotlin MainActivity files are not supported yet.`
+      config.modResults.contents = modifyKotlinMainActivity(
+        config.modResults.contents
       )
     }
     return config
